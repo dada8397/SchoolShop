f1422ead42754918fa43c1dbb27c4d7e19b5c323
dadachan <vincent8397@gmail.com>
2019-06-03 12:04:37 +0800

    Initialize all pages

--- app/src/main/java/com/example/schoolshop/MainActivity.java
+++ app/src/main/java/com/example/schoolshop/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.schoolshop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void listOnClick(View v) {
+        Intent intent = new Intent(MainActivity.this, ListActivity.class);
+        startActivity(intent);
+    }
+
+    public void createOnClick(View v) {
+        Intent intent = new Intent(MainActivity.this, CreateActivity.class);
+        startActivity(intent);
+    }
+
+    public void detailOnClick(View v) {
+        Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+        startActivity(intent);
+    }
+
+    public void chatOnClick(View v) {
+        Intent intent = new Intent(MainActivity.this, ChatActivity.class);
+        startActivity(intent);
     }
 }
