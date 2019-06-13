4c86dc0a9788ce203f7a17ca25fda8e274f23313
dadachan <vincent8397@gmail.com>
2019-06-14 00:20:45 +0800

    Finish apis

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Stuff(models.Model):
+    name = models.TextField(blank=True)
+    owner = models.TextField(blank=True)
+    description = models.TextField(blank=True)
+    img_url = models.TextField(blank=True)
+    price = models.TextField(blank=True)
+    status = models.TextField(blank=True)
+    created_at = models.TextField(blank=True)
+    updated_at = models.TextField(blank=True)
+
+class Chat(models.Model):
+    src = models.TextField(blank=True)
+    dst = models.TextField(blank=True)
+    content = models.TextField(blank=True)
+    time = models.TextField(blank=True)
--- backend/proj/urls.py
+++ backend/proj/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('createStuff/', createStuff),
+    path('getStuffs/', getStuffs),
+    path('delStuff/', delStuff),
+    path('setStuffBuying/', setStuffBuying),
+    path('setStuffSoldout/', setStuffSoldout),
+    path('sendMsg/', sendMsg),
+    path('getMsgs/', getMsgs),
 ]
