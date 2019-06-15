1537b7585baed695b74db7901590068e527b319c
Shani_Hsieh <sunny3030730@gmail.com>
2019-06-15 02:27:23 -0400

    refine CreatActivity.java

--- app/src/main/java/com/example/schoolshop/CreateActivity.java
+++ app/src/main/java/com/example/schoolshop/CreateActivity.java
@@ -3,7 +3,9 @@ package com.example.schoolshop;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.nfc.Tag;
 import android.os.Bundle;
@@ -13,6 +15,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -32,6 +35,8 @@ import okhttp3.Response;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,9 +44,16 @@ import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class CreateActivity extends AppCompatActivity {
 
-    private ImageView imageView1;
-    private ImageView imageView2;
+    private ImageView image1;
+    private ImageView image2;
     private Button submit;
+    private EditText item;
+    private EditText price;
+    private EditText description;
+    private String get_item;
+    private String get_price;
+    private String get_description;
+
 
     public String postUrl= "http://merry.ee.ncku.edu.tw:10000/createStuff/";
     public String postBody="{\"name\": \"500G SSD\", \"owner\": \"1\", \"description\": \"9成新，用過一個月，附發票\", \"img_url\": {\"1\": \"https://i.imgur.com/3JIbb5O.jpg\", \"2\": \"https://i.imgur.com/OfGYgqk.png\"}, \"price\": \"1000\"}";
@@ -55,13 +67,19 @@ public class CreateActivity extends AppCompatActivity {
 
 
         //初始化
-        imageView1 = (ImageView) findViewById(R.id.photo1_imageView);
-        imageView2 = (ImageView) findViewById(R.id.photo2_imageView);
+        image1 = (ImageView) findViewById(R.id.photo1_imageView);
+        image2 = (ImageView) findViewById(R.id.photo2_imageView);
         submit = (Button) findViewById(R.id.submit_button);
+
+        item = (EditText) findViewById(R.id.item_editText);
+        price = (EditText) findViewById(R.id.price_editText);
+        description = (EditText) findViewById(R.id.description_editText);
+
+
 
 
         //擷取照片按鈕監聽器
-        imageView1.setOnClickListener(new Button.OnClickListener() {
+        /*image1.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //讀取圖片
@@ -75,7 +93,7 @@ public class CreateActivity extends AppCompatActivity {
                 startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1);
             }
         });
-        imageView2.setOnClickListener(new Button.OnClickListener() {
+        image2.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //讀取圖片
@@ -90,19 +108,56 @@ public class CreateActivity extends AppCompatActivity {
             }
 
 
+        });*/
+
+
+
+        //擷取照片按鈕監聽器
+        image1.setOnClickListener(new Button.OnClickListener() {
+            String url1 = "https://i.imgur.com/3JIbb5O.jpg";
+
+
+            @Override
+            public void onClick(View v) {
+                LoadImageFromWebOperations(url1);
+                image1.setImageDrawable(LoadImageFromWebOperations(url1));
+
+            }
+        });
+        image2.setOnClickListener(new Button.OnClickListener() {
+
+            String url2 = "https://i.imgur.com/OfGYgqk.png";
+            @Override
+            public void onClick(View v) {
+                //LoadImageFromWebOperations(url2);
+                //Bitmap bmp = BitmapFactory.decodeStream(url2.openConnection().getInputStream());
+                //image2.setImageDrawable(LoadImageFromWebOperations(url2));
+                image2.setImageDrawable(LoadImageFromWebOperations(url2));
+
+            }
+
+
         });
 
         submit.setOnClickListener(new Button.OnClickListener() {
 
             @Override
             public void onClick(View v) {
+
+                get_item = item.getText().toString();
+                get_price = price.getText().toString();
+                get_description = description.getText().toString();
+
+                postBody = "{\"name\": \"" + get_item + "\", \"owner\": \"1\", \"description\": \"" + get_description + "\", \"img_url\": {\"1\": \"https://i.imgur.com/3JIbb5O.jpg\", \"2\": \"https://i.imgur.com/OfGYgqk.png\"}, \"price\": \"" + get_price + "\"}";
+
                 /*try {
                     postRequest(postUrl, postBody);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }*/
+
                 Intent intent = new Intent(CreateActivity.this, DetailActivity.class);
-                intent.putExtra(EXTRA_MESSAGE, postBody);
+                intent.putExtra("ITEM", postBody);
                 startActivity(intent);
             }
         });
@@ -133,26 +188,37 @@ public class CreateActivity extends AppCompatActivity {
         });
     }
 
+    public static Drawable LoadImageFromWebOperations(String url) {
+        try {
+            InputStream is = (InputStream) new URL(url).getContent();
+            Drawable d = Drawable.createFromStream(is, "src name");
+            return d;
+
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
 
 
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Log.e("456", "eeeeeeeeeeeee");
+        //Log.e("456", "eeeeeeeeeeeee");
         super.onActivityResult(requestCode, resultCode, data);
 
         if (resultCode == RESULT_OK && data != null && data.getData() != null) {
-            Log.e("123", "fffffffffffffffff");
+            //Log.e("123", "fffffffffffffffff");
 
             Uri uri = data.getData();
             try {
                 if (requestCode == 1) {
                     Bitmap bitmap1 = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-                    imageView1.setImageBitmap(bitmap1);
+                    image1.setImageBitmap(bitmap1);
                 }
                 else if (requestCode == 2) {
                     Bitmap bitmap2 = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-                    imageView2.setImageBitmap(bitmap2);
+                    image2.setImageBitmap(bitmap2);
                 }
 
             } catch (IOException e) {
@@ -162,3 +228,4 @@ public class CreateActivity extends AppCompatActivity {
     }
 
 }
+
